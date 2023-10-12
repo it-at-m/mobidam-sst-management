4dd07135800ea6ac24ab5bea3ca9399ea997e4f1
Angelina Hofmann <angelina.hofmann@muenchen.de>
2023-10-12 11:03:46 +0200

    :recycle: remove unused code

--- frontend/src/App.vue
+++ frontend/src/App.vue
@@ -9,37 +9,14 @@
         >
             <v-row align="center">
                 <v-col
-                    cols="3"
+                    cols="2"
                     class="d-flex align-center justify-start"
                 >
-                    <v-app-bar-nav-icon @click.stop="drawer = !drawer" />
-                    <router-link to="/">
-                        <v-toolbar-title class="font-weight-bold">
-                            <span class="white--text">RefArch-</span>
-                            <span class="secondary--text">Kick</span>
-                            <span class="white--text">Starter</span>
-                        </v-toolbar-title>
-                    </router-link>
+                    <span class="font-weight-bold text-h5">MobidaM</span>
                 </v-col>
+                <v-spacer />
                 <v-col
-                    cols="6"
-                    class="d-flex align-center justify-center"
-                >
-                    <v-text-field
-                        id="suchfeld"
-                        v-model="query"
-                        flat
-                        solo-inverted
-                        hide-details
-                        label="Suche"
-                        clearable
-                        prepend-inner-icon="mdi-magnify"
-                        color="black"
-                        @keyup.enter="search"
-                    />
-                </v-col>
-                <v-col
-                    cols="3"
+                    cols="2"
                     class="d-flex align-center justify-end"
                 >
                     <v-btn
@@ -53,19 +30,6 @@
                 </v-col>
             </v-row>
         </v-app-bar>
-        <v-navigation-drawer
-            v-model="drawer"
-            app
-            clipped
-        >
-            <v-list>
-                <v-list-item :to="{ path: '/getstarted' }">
-                    <v-list-item-content>
-                        <v-list-item-title>Get started</v-list-item-title>
-                    </v-list-item-content>
-                </v-list-item>
-            </v-list>
-        </v-navigation-drawer>
         <v-main>
             <v-container fluid>
                 <v-fade-transition mode="out-in">
@@ -82,7 +46,6 @@ import { onMounted, ref, watch } from "vue";
 import { useRoute } from "vue-router/composables";
 import { useSnackbarStore } from "@/stores/snackbar";
 
-const drawer = ref(true);
 const query = ref("");
 const appswitcherBaseUrl = ref<string | null>(null);
 
@@ -110,15 +73,6 @@ watch(
         }
     }
 );
-
-//Navigiert zur Seite mit den Suchergebnissen und sendet ein Event zum Auslösen weiterer Suchen.
-async function search(): Promise<void> {
-    if (query.value !== "" && query.value !== null) {
-        snackbarStore.showMessage({
-            message: "Sie haben nach " + query.value + " gesucht. ;)",
-        });
-    }
-}
 </script>
 
 <style>
--- frontend/src/views/MainView.vue
+++ frontend/src/views/MainView.vue
@@ -1,18 +1,9 @@
 <template>
     <v-container>
         <v-row class="text-center">
-            <v-col cols="12">
-                <v-img
-                    :src="require('../assets/logo.png')"
-                    class="my-3"
-                    contain
-                    height="200"
-                />
-            </v-col>
-
             <v-col class="mb-4">
                 <h1 class="text-h3 font-weight-bold mb-3">
-                    Willkommen beim RefArch-Kickstarter
+                    Willkommen bei MobidaM
                 </h1>
                 <p>
                     Das API-Gateway ist:
