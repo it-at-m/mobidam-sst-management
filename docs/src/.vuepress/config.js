7e726125a82f9fec670bb22e92a9d067453fc325
martin.dietrich <martin.dietrich@muenchen.de>
2023-10-23 12:17:21 +0200

    📝 vuepress added

---
+++ docs/src/.vuepress/config.js
@@ -0,0 +1,84 @@
+const { description } = require('../../package')
+
+module.exports = {
+  /**
+   * Ref：https://v1.vuepress.vuejs.org/config/#title
+   */
+  title: 'MobidaM',
+  /**
+   * Ref：https://v1.vuepress.vuejs.org/config/#description
+   */
+  description: description,
+
+  /**
+   * Extra tags to be injected to the page HTML `<head>`
+   *
+   * ref：https://v1.vuepress.vuejs.org/config/#head
+   */
+  head: [
+    ['meta', { name: 'theme-color', content: '#333333' }],
+    ['meta', { name: 'apple-mobile-web-app-capable', content: 'yes' }],
+    ['meta', { name: 'apple-mobile-web-app-status-bar-style', content: 'black' }]
+  ],
+
+  /**
+   * Theme configuration, here is the default theme configuration for VuePress.
+   *
+   * ref：https://v1.vuepress.vuejs.org/theme/default-theme-config.html
+   */
+  themeConfig: {
+    repo: 'https://github.com/it-at-m/mobidam-sst-management',
+    editLinks: false,
+    docsDir: '',
+    editLinkText: '',
+    lastUpdated: false,
+    nav: [
+      {
+        text: 'Dokumentation',
+        link: '/dokumentation/',
+      },
+      {
+        text: 'Konfiguration',
+        link: '/konfiguration/'
+      }
+    ],
+    sidebar: {
+      '/dokumentation/': [
+        {
+          title: 'Dokumentation',
+          collapsable: false,
+          children: [
+            '',
+            'using-vue',
+          ]
+        }
+      ],
+      "/configuration/": [
+        {
+          title: "Umgebung",
+          collapsable: false,
+          children: ["env/"],
+        }
+      ]
+    }
+  },
+
+  /**
+   * markdown extension
+   */
+  markdown: {
+    lineNumbers: true,
+    extendMarkdown: (md) => {
+      md.use(require("markdown-it-footnote"));
+    },
+  },
+
+  /**
+   * Apply plugins，ref：https://v1.vuepress.vuejs.org/zh/plugin/
+   */
+  plugins: [
+    '@vuepress/plugin-back-to-top',
+    '@vuepress/plugin-medium-zoom',
+    'vuepress-plugin-mermaidjs'
+  ]
+}
