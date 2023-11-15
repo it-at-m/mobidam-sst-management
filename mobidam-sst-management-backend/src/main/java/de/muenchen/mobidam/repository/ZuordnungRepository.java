403eede02a0464f894094eac2bb6de8b704df8f8
Angelina Hofmann <angelina.hofmann@muenchen.de>
2023-11-15 11:28:15 +0100

    :refactor: simplify

--- mobidam-sst-management-backend/src/main/java/de/muenchen/mobidam/repository/ZuordnungRepository.java
+++ mobidam-sst-management-backend/src/main/java/de/muenchen/mobidam/repository/ZuordnungRepository.java
@@ -5,9 +5,7 @@ import de.muenchen.mobidam.domain.Zuordnung;
 import java.util.List;
 import java.util.UUID;
 import org.springframework.data.repository.CrudRepository;
-import org.springframework.stereotype.Repository;
 
-@Repository
 public interface ZuordnungRepository extends CrudRepository<Zuordnung, UUID> {
     List<Zuordnung> findZuordnungsBySchnittstelle(String schnittstelle);
 }
