3f56475047c68fe8594fe2e538e8fdbbbfcd5f9a
Mirna Rodic <mirna.rodic@muenchen.de>
2024-01-24 17:00:47 +0100

    :rotating_light: Style check

--- mobidam-sst-management-backend/src/main/java/de/muenchen/mobidam/domain/dtos/DatentransferDTO.java
+++ mobidam-sst-management-backend/src/main/java/de/muenchen/mobidam/domain/dtos/DatentransferDTO.java
@@ -2,13 +2,12 @@ package de.muenchen.mobidam.domain.dtos;
 
 import jakarta.validation.constraints.NotEmpty;
 import jakarta.validation.constraints.NotNull;
+import java.time.LocalDateTime;
+import java.util.UUID;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
 import lombok.Setter;
 import lombok.ToString;
-
-import java.time.LocalDateTime;
-import java.util.UUID;
 
 @Getter
 @Setter
--- mobidam-sst-management-backend/src/main/java/de/muenchen/mobidam/domain/mappers/DatentransferMapper.java
+++ mobidam-sst-management-backend/src/main/java/de/muenchen/mobidam/domain/mappers/DatentransferMapper.java
@@ -2,8 +2,10 @@ package de.muenchen.mobidam.domain.mappers;
 
 import de.muenchen.mobidam.domain.Datenstransfer;
 import de.muenchen.mobidam.domain.dtos.DatentransferDTO;
+import org.mapstruct.Mapper;
 import org.mapstruct.Mapping;
 
+@Mapper
 public interface DatentransferMapper {
 
     @Mapping(target = "schnittstelle", source = "schnittstelle.id")
