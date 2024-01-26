078b590c3ce157030e1b018a1e91015ebd5778ee
Mirna Rodic <mirna.rodic@muenchen.de>
2024-01-26 09:42:54 +0100

    :rotating_light: Fixed Checkstyle

--- mobidam-sst-management-backend/src/main/java/de/muenchen/mobidam/repository/DatentransferRepository.java
+++ mobidam-sst-management-backend/src/main/java/de/muenchen/mobidam/repository/DatentransferRepository.java
@@ -3,7 +3,6 @@ package de.muenchen.mobidam.repository;
 import de.muenchen.mobidam.domain.Datentransfer;
 import java.util.List;
 import java.util.UUID;
-
 import org.springframework.data.repository.CrudRepository;
 
 public interface DatentransferRepository extends CrudRepository<Datentransfer, UUID> {
