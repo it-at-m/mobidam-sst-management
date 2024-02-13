307abe68b77a975afc75ae05996e13cf404f6d8b
Mirna Rodic <mirna.rodic@muenchen.de>
2024-02-13 11:56:27 +0100

    :recycle: Checkstyle fix

--- mobidam-sst-management-backend/src/main/java/de/muenchen/mobidam/repository/SchnittstelleRepository.java
+++ mobidam-sst-management-backend/src/main/java/de/muenchen/mobidam/repository/SchnittstelleRepository.java
@@ -23,7 +23,6 @@
 package de.muenchen.mobidam.repository;
 
 import de.muenchen.mobidam.domain.Schnittstelle;
-
 import java.util.List;
 import java.util.UUID;
 import org.springframework.data.repository.CrudRepository;
