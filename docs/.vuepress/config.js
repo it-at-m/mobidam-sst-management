3bf47a922fe1f84aff59aaff0838d72fd546bdd7
martin.dietrich <martin.dietrich@muenchen.de>
2024-03-01 08:07:44 +0100

    base rem.

--- docs/.vuepress/config.js
+++ docs/.vuepress/config.js
@@ -9,7 +9,6 @@ module.exports = {
    * Ref：https://v1.vuepress.vuejs.org/config/#description
    */
   description: description,
-  base: '/mobidam-sst-management/',
 
   /**
    * Extra tags to be injected to the page HTML `<head>`
