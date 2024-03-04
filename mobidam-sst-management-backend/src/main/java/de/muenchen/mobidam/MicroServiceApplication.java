66253d39ffebc77b1953691a5348e1a4ff62c0c4
martin.dietrich <martin.dietrich@muenchen.de>
2024-03-04 16:05:30 +0100

    dgb sysout

--- mobidam-sst-management-backend/src/main/java/de/muenchen/mobidam/MicroServiceApplication.java
+++ mobidam-sst-management-backend/src/main/java/de/muenchen/mobidam/MicroServiceApplication.java
@@ -22,6 +22,7 @@
  */
 package de.muenchen.mobidam;
 
+import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
 import org.springframework.boot.autoconfigure.domain.EntityScan;
@@ -51,10 +52,14 @@ import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
         }
 )
 @EnableAutoConfiguration
-public class MicroServiceApplication {
+public class MicroServiceApplication implements CommandLineRunner {
 
     public static void main(String[] args) {
         SpringApplication.run(MicroServiceApplication.class, args);
     }
 
+    @Override
+    public void run(String... args) throws Exception {
+        System.out.println("Branch: test/sso");
+    }
 }
